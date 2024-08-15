f9f070a2845567d977a76107736a7460b2562c28
Naeem Ashraf <123843315+NP5555@users.noreply.github.com>
2024-08-15 00:09:26 -0700

    "added routes"

--- routes/UserRoutes.js
+++ routes/UserRoutes.js
@@ -3,7 +3,6 @@ const express = require('express');
 // const { getUsers } = require('../controllers/userController');
 const router = express.Router();
 const { getUsers, addUser, loginUser } = require('../controllers/userController');
-
 router.get('/users', getUsers);
 router.post('/users', addUser);
 router.post('/users/login', loginUser);
